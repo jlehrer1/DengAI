fd71505ec062a74c54e7481648e3034a00dafce4
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-03 17:07:00 -0700

    File structure, preprocessing error handling

--- output/preprocess.py
+++ output/preprocess.py
@@ -3,14 +3,13 @@ import numpy as np
 import sklearn as sk
 from sklearn.impute import KNNImputer
 
-def impute_df(df):
-    imputer = KNNImputer()
+def impute_df(df, imputer):
     for column in df.columns:
         if df[column].isna().sum() != 0:
             df[column] = imputer.fit_transform(df[[column]])
     return df
 
-if __name__ == "__main__":
+def process_data(imputer):
     df = pd.read_csv('../data/raw/dengue_features_train.csv')
     df_labels = pd.read_csv('../data/raw/dengue_labels_train.csv')
     df_test = pd.read_csv('../data/raw/dengue_features_test.csv')
@@ -18,6 +17,12 @@ if __name__ == "__main__":
     df = impute_df(df)
     df_labels = impute_df(df_labels)
     df_test = impute_df(df_test)
+
+    try:
+        df.drop(labels=['precipitation_amt_mm', 'reanalysis_specific_humidity_g_per_kg', 'reanalysis_min_air_temp_k'], axis=1, inplace=True)
+        df_test.drop(labels=['precipitation_amt_mm', 'reanalysis_specific_humidity_g_per_kg', 'reanalysis_min_air_temp_k'], axis=1, inplace=True)
+    except KeyError:
+        print('Columns already dropped. Continuing...')
 
     sj_train_features = df.loc[df['city'] == 'sj']
     iq_train_features = df.loc[df['city'] == 'sj']
@@ -32,3 +37,7 @@ if __name__ == "__main__":
     sj_train_labels.to_csv('../data/clean/sj/sj_train_labels.csv', index=False)
     iq_train_features.to_csv('../data/clean/iq/iq_train_features.csv', index=False)
     iq_train_labels.to_csv('../data/clean/iq/iq_train_labels.csv', index=False)
+
+if __name__ == "__main__":
+    imputer = KNNImputer()
+    process_data(imputer)
